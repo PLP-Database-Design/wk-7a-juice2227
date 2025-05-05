996af450f1b370d05358186a42138ca14c715e4c
juice2227 <myceline2227@gmail.com>
2025-05-05 10:37:48 +0300

    fixing the queries

--- answers.sql
+++ answers.sql
@@ -1,57 +1,25 @@
-USE salesdb;
-
--- question 1
 CREATE TABLE
     ProductDetail (
         OrderID INT,
-        CustomerName VARCHAR(255),
-        Products VA RCHAR (255)
-    )
+        CustomerName VARCHAR(100),
+        Products VARCHAR(100)
+    );
+
 INSERT INTO
     ProductDetail (OrderID, CustomerName, Products)
 VALUES
-    ('101', 'John Doe ', 'Laptop ,Mouse '),
-    ('102', 'Jane Smith', 'Tablet, Keyboard, Mouse'),
-    ('103', 'Emily Clark', 'Phone')
-INSERT INTO
-    ProductDetail (OrderID, CustomerName, Products)
-VALUES
-    ('101', 'John Doe ', 'Mouse '),
-    ('102', 'Jane Smith', 'Tablet'),
-    ('103', 'Emily Clark', 'Phone'),
-    ('101', 'John Doe ', 'Laptop '),
-    ('102', 'Jane Smith', 'Keyboard'),
-    ('102', 'Jane Smith', 'Mouse')
-    -- question 2
-CREATE TABLE
-    OrderDetails (
-        OrderID INT,
-        CustomerName VARCHAR(255),
-        Product VARCHAR(255),
-        Quantity INT
-    )
-INSERT INTO
-    OrderDetails (OrderID, CustomerName, Product, Quantity)
-VALUES
-    ('101', 'John Doe', 'Laptop', '2'),
-    ('101', 'John Doe', 'Mouse', '2'),
-    ('102', 'Jane Smith', 'Tablet', '3'),
-    ('102', 'Jane Smith ', 'Keyboard ', '1'),
-    ('102', 'Jane Smith', 'Mouse', '2'),
-    ('103', 'Emily Clark ', 'Phone', '1')
+    (101, 'John Doe', 'Laptop'),
+    (101, 'John Doe', 'Mouse'),
+    (102, 'Jane Smith', 'Tablet'),
+    (102, 'Jane Smith', 'Keyboard'),
+    (102, 'Jane Smith', 'Mouse'),
+    (103, 'Emily Clark', 'Phone');
+
+-- Question 2
 CREATE TABLE
     Orders (
         OrderID INT PRIMARY KEY,
-        CustomerName VARCHAR(255)
-    );
-
-CREATE TABLE
-    OrderDetails (
-        OrderID INT,
-        Product VARCHAR(255),
-        Quantity INT,
-        PRIMARY KEY (OrderID, Product),
-        FOREIGN KEY (OrderID) REFERENCES Orders (OrderID)
+        CustomerName VARCHAR(100)
     );
 
 INSERT INTO
@@ -61,8 +29,17 @@ VALUES
     (102, 'Jane Smith'),
     (103, 'Emily Clark');
 
+CREATE TABLE
+    Product (
+        OrderID INT,
+        Product VARCHAR(100),
+        Quantity INT,
+        PRIMARY KEY (OrderID, Product),
+        FOREIGN KEY (OrderID) REFERENCES Orders (OrderID)
+    );
+
 INSERT INTO
-    OrderDetails (OrderID, Product, Quantity)
+    Product (OrderID, Product, Quantity)
 VALUES
     (101, 'Laptop', 2),
     (101, 'Mouse', 1),
